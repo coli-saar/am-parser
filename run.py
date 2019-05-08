3df2eb5c7106a97289ed5fd49d45c71be6f162f8
namednil <mlinde@coli.uni-saarland.de>
2019-05-08 13:10:10 +0200

    Added necessary import.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import tempfile
 
+import graph_dependency_parser.graph_dependency_parser #important import
+
 from allennlp.commands.train import train_model
 
 from allennlp.common.params import Params
