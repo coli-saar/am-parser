3d907ac38fbca587aa7084a3bdd0906434b745b8
Jonas Groschwitz <jonasg@login.coli.uni-saarland.de>
2021-01-13 16:34:42 +0100

    added some SDP training scripts

---
+++ scripts/unsupervised2020/trainSDP_EM_0iter3.sh
@@ -0,0 +1,17 @@
+amconllfolder="/proj/irtg/sempardata/unsupervised2020/amconll/EM_0iter3"
+modelprefix="EM_0iter3"
+comettag="EM_0iter3"
+
+modellocation="/local/jonasg/unsupervised2020/models"
+
+mkdir -p $modellocation/$modelprefix/DM/
+python -u train.py jsonnets/single/bert/DM.jsonnet -s $modellocation/$modelprefix/DM  -f --file-friendly-logging  -o "{\"trainer\" : {\"cuda_device\" :  0 }  , \"train_data_path\": [ [\"DM\", \"$amconllfolder/DM/train.amconll\"]] , \"validation_data_path\": [ [\"DM\", \"$amconllfolder/DM/dev.amconll\"]] }" --comet Yt3xk2gaFeevDwlxSNzN2VUKh --tags $comettag DM --project unsupervised2020 --workspace jgroschwitz &> $modellocation/$modelprefix/DM/log.txt &
+
+mkdir -p $modellocation/$modelprefix/PAS/
+python -u train.py jsonnets/single/bert/PAS.jsonnet -s $modellocation/$modelprefix/PAS  -f --file-friendly-logging  -o "{\"trainer\" : {\"cuda_device\" :  1 }  , \"train_data_path\": [ [\"PAS\", \"$amconllfolder/PAS/train.amconll\"]] , \"validation_data_path\": [ [\"PAS\", \"$amconllfolder/PAS/dev.amconll\"]] }" --comet Yt3xk2gaFeevDwlxSNzN2VUKh --tags $comettag PAS --project unsupervised2020 --workspace jgroschwitz &> $modellocation/$modelprefix/PAS/log.txt &
+
+mkdir -p $modellocation/$modelprefix/PSD/
+python -u train.py jsonnets/single/bert/PSD.jsonnet -s $modellocation/$modelprefix/PSD -f --file-friendly-logging  -o "{\"trainer\" : {\"cuda_device\" :  2 }  , \"train_data_path\": [ [\"PSD\", \"$amconllfolder/PSD/train.amconll\"]] , \"validation_data_path\": [ [\"PSD\", \"$amconllfolder/PSD/dev.amconll\"]] }" --comet Yt3xk2gaFeevDwlxSNzN2VUKh --tags $comettag PSD --project unsupervised2020 --workspace jgroschwitz &> $modellocation/$modelprefix/PSD/log.txt &
+
+wait
+
